f0b79d049d811511fa138e1c822af887c2aac0fe
NoorTushar <tushar.noor.khan@gmail.com>
2024-06-29 22:01:33 +0600

    will work on api now

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,20 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import ProfileCard from "./Components/ProfileCard/ProfileCard";
+import ProfileDetailsCard from "./Components/ProfileDetailsCard/ProfileDetailsCard";
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+   return (
+      <div className="p-10">
+         <h3>Users : 10</h3>
+         <div className="grid grid-cols-12 gap-6">
+            <div className="col-span-3">
+               <ProfileCard />
+            </div>
+            <div className="col-span-9">
+               <ProfileDetailsCard />
+            </div>
+         </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+   );
 }
 
-export default App
+export default App;

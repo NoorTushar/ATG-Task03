0be34e016ea79122f365bc1cd8c7a5e29d6dc2bd
NoorTushar <tushar.noor.khan@gmail.com>
2024-06-30 20:53:57 +0600

    new start

---
+++ src/Components/Card/Card.jsx
@@ -0,0 +1,24 @@
+import dummyImage from "../../assets/default-pic.png";
+const Card = ({ user }) => {
+   return (
+      <div className="flex items-center gap-4 p-8 bg-slate-800 m-4 rounded-lg shadow shadow-slate-400 hover:shadow-violet-500 duration-300 cursor-pointer group">
+         <img
+            src={user?.avatar}
+            alt={user?.profile?.username}
+            className="size-16 rounded-full"
+            onError={(e) => {
+               e.target.onerror = null;
+               e.target.src = dummyImage;
+            }}
+         />
+         <div className="">
+            <h2 className="text-xl text-primary group-hover:text-violet-500 duration-300">
+               {user?.profile?.username}
+            </h2>
+            <p className="text-sm">{user?.jobTitle}</p>
+         </div>
+      </div>
+   );
+};
+
+export default Card;
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,6 @@ export default {
 
    plugins: [require("daisyui")],
    daisyui: {
-      themes: ["light", "synthwave"],
+      themes: ["light", "dark"],
    },
 };

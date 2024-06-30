6eac571f096569cb2bb7088eea573433087ecaaf
NoorTushar <tushar.noor.khan@gmail.com>
2024-07-01 01:03:29 +0600

    i think done

--- src/App.jsx
+++ src/App.jsx
@@ -6,111 +6,134 @@ import LoadingSpinner from "./Components/Shared/LoadingSpinner";
 import dummyImage from "./assets/person-dummy.jpg";
 
 const App = () => {
-   const [users, isLoading] = useAllUsers();
+   const [users, isLoading, isError] = useAllUsers();
    const [selectedUser, setSelectedUser] = useState(null);
    const [openModal, setOpenModal] = useState(false);
+   const [isImageLoading, setIsImageLoading] = useState(true);
 
    const handleShowDetails = (user) => {
       console.log(user);
       setSelectedUser(user);
       setOpenModal(true);
+      setIsImageLoading(true);
    };
 
    if (isLoading) return <LoadingSpinner />;
+
    return (
       <div className="">
          <h3 className="text-xl p-6 border-b border-b-slate-400">
             Total Users : {users?.length > 0 ? users?.length : "0"}
          </h3>
-         <div className="grid grid-cols-12 relative">
-            <div className="lg:col-span-5 col-span-12 max-h-[calc(100vh-78px)] overflow-scroll">
-               {users.map((user) => (
-                  <Card
-                     key={user?.profile?.email}
-                     user={user}
-                     handleShowDetails={handleShowDetails}
-                  />
-               ))}
-            </div>
-            <div className="lg:col-span-7 flex items-center justify-center z-10 bg-transparent bottom-0 mx-auto  ">
-               {!openModal && (
-                  <h3 className="hidden lg:block">
-                     Click any card to view details
-                  </h3>
-               )}
 
-               {selectedUser && openModal && (
-                  <div className="space-y-1 text-primary lg:relative absolute right-0 lg:top-0 lg:right-0 top-1/3 left-0 bottom-0 shadow-md shadow-violet-500 p-4 sm:p-8 lg:p-10 bg-black break-words rounded-lg border border-violet-500">
-                     <div className="mb-4 relative flex justify-center ">
-                        <img
-                           src={selectedUser?.avatar}
-                           alt={selectedUser?.profile?.username}
-                           className="size-20 rounded-full"
-                           onError={(e) => {
-                              e.target.onerror = null;
-                              e.target.src = dummyImage;
-                           }}
-                        />
-                        <button
-                           onClick={() => setOpenModal(false)}
-                           className="absolute right-5 top-0"
-                        >
-                           <IoMdClose className="text-2xl" />
-                        </button>
+         {isError && (
+            <h3 className="text-xl p-6 border-b border-b-slate-400">
+               No data to show at the moment
+            </h3>
+         )}
+         {!isError && (
+            <div className="grid grid-cols-12 relative">
+               <div className="lg:col-span-5 col-span-12 max-h-[calc(100vh-78px)] overflow-scroll">
+                  {users.map((user) => (
+                     <Card
+                        key={user?.profile?.email}
+                        user={user}
+                        handleShowDetails={handleShowDetails}
+                     />
+                  ))}
+               </div>
+               <div className="lg:col-span-7  flex items-center justify-center z-10 bg-transparent bottom-0 mx-auto  ">
+                  {!openModal && (
+                     <h3 className="hidden lg:block">
+                        Click any card to view details
+                     </h3>
+                  )}
+
+                  {selectedUser && openModal && (
+                     <div className="space-y-1 lg:w-[500px] lg:h-[400px] text-primary lg:relative absolute right-0 lg:top-0 lg:right-0 top-[45%] left-0 bottom-0 shadow-lg shadow-violet-500 p-10  bg-black break-words rounded-lg border border-violet-500">
+                        <div className="mb-4 relative flex justify-center ">
+                           {isImageLoading && (
+                              <img
+                                 src={dummyImage}
+                                 alt="Loading..."
+                                 className="size-20 rounded-full object-cover"
+                              />
+                           )}
+                           <img
+                              src={selectedUser?.avatar}
+                              alt={selectedUser?.profile?.username}
+                              className={`size-20 object-cover rounded-full ${
+                                 isImageLoading ? "hidden" : "block"
+                              }`}
+                              onLoad={() => setIsImageLoading(false)}
+                              onError={(e) => {
+                                 e.target.onerror = null;
+                                 e.target.src = dummyImage;
+                                 setIsImageLoading(false);
+                              }}
+                           />
+                           <button
+                              onClick={() => setOpenModal(false)}
+                              className="absolute right-5 top-0"
+                           >
+                              <IoMdClose className="text-2xl" />
+                           </button>
+                        </div>
+                        <h2 className="font-medium">
+                           Username:{" "}
+                           <span className="text-gray-400 font-normal">
+                              {selectedUser?.profile?.username ||
+                                 "No data to show"}
+                           </span>
+                        </h2>
+                        <p className="font-medium">
+                           First Name:{" "}
+                           <span className="text-gray-400 font-normal">
+                              {selectedUser?.profile?.firstName ||
+                                 "No data to show"}
+                           </span>
+                        </p>
+                        <p className="font-medium">
+                           Last Name:{" "}
+                           <span className="text-gray-400 font-normal">
+                              {selectedUser?.profile?.lastName ||
+                                 "No data to show"}
+                           </span>
+                        </p>
+                        <p className="font-medium">
+                           Email:{" "}
+                           <span className="text-gray-400 font-normal">
+                              {selectedUser?.profile?.email ||
+                                 "No data to show"}
+                           </span>
+                        </p>
+                        <p className="font-medium">
+                           User Created At:{" "}
+                           <span className="text-gray-400 font-normal">
+                              {new Date(
+                                 selectedUser?.createdAt
+                              ).toLocaleDateString() || "No data to show"}
+                           </span>
+                        </p>
+                        <p className="font-medium">
+                           Bio:
+                           <span className="text-gray-400 font-normal">
+                              {" "}
+                              {selectedUser?.Bio || "No data to show"}
+                           </span>
+                        </p>
+                        <p className="font-medium">
+                           Job Title:
+                           <span className="text-gray-400 font-normal">
+                              {" "}
+                              {selectedUser?.jobTitle || "No data to show"}
+                           </span>
+                        </p>
                      </div>
-                     <h2 className="font-medium">
-                        Username:{" "}
-                        <span className="text-gray-400 font-normal">
-                           {selectedUser?.profile?.username ||
-                              "No data to show"}
-                        </span>
-                     </h2>
-                     <p className="font-medium">
-                        First Name:{" "}
-                        <span className="text-gray-400 font-normal">
-                           {selectedUser?.profile?.firstName ||
-                              "No data to show"}
-                        </span>
-                     </p>
-                     <p className="font-medium">
-                        Last Name:{" "}
-                        <span className="text-gray-400 font-normal">
-                           {selectedUser?.profile?.lastName ||
-                              "No data to show"}
-                        </span>
-                     </p>
-                     <p className="font-medium">
-                        Email:{" "}
-                        <span className="text-gray-400 font-normal">
-                           {selectedUser?.profile?.email || "No data to show"}
-                        </span>
-                     </p>
-                     <p className="font-medium">
-                        User Created At:{" "}
-                        <span className="text-gray-400 font-normal">
-                           {new Date(
-                              selectedUser?.createdAt
-                           ).toLocaleDateString() || "No data to show"}
-                        </span>
-                     </p>
-                     <p className="font-medium">
-                        Bio:
-                        <span className="text-gray-400 font-normal">
-                           {" "}
-                           {selectedUser?.Bio || "No data to show"}
-                        </span>
-                     </p>
-                     <p className="font-medium">
-                        Job Title:
-                        <span className="text-gray-400 font-normal">
-                           {" "}
-                           {selectedUser?.jobTitle || "No data to show"}
-                        </span>
-                     </p>
-                  </div>
-               )}
+                  )}
+               </div>
             </div>
-         </div>
+         )}
       </div>
    );
 };
--- src/Components/Card/Card.jsx
+++ src/Components/Card/Card.jsx
@@ -1,18 +1,33 @@
 /* eslint-disable react/prop-types */
+import { useState } from "react";
 import dummyImage from "../../assets/person-dummy.jpg";
+
 const Card = ({ user, handleShowDetails }) => {
+   const [isImageLoading, setIsImageLoading] = useState(true);
+
    return (
       <div
          onClick={() => handleShowDetails(user)}
          className="flex items-center gap-4 p-8 bg-slate-800 m-4 rounded-lg shadow shadow-slate-400 hover:bg-slate-900 hover:shadow-violet-500 duration-300 cursor-pointer group"
       >
+         {isImageLoading && (
+            <img
+               src={dummyImage}
+               alt="Loading..."
+               className="size-16 rounded-full"
+            />
+         )}
          <img
             src={user?.avatar}
             alt={user?.profile?.username}
-            className="size-16 rounded-full"
+            className={`size-16 rounded-full ${
+               isImageLoading ? "hidden" : "block"
+            }`}
+            onLoad={() => setIsImageLoading(false)}
             onError={(e) => {
                e.target.onerror = null;
                e.target.src = dummyImage;
+               setIsImageLoading(false);
             }}
          />
          <div className="">
